507237e3a26468d11f758eb9189f27b7a498558f
François Liard <56561415+Pacdac@users.noreply.github.com>
2024-02-14 13:55:52 +0100

    Setup basic ERC20

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -1,2 +1,8 @@
 //License: None
 pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract PGLABTOKEN is ERC20 {
+    constructor() ERC20("PGLABTOKEN", "PGLT") {}
+}
