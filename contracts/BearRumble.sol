19bc12ce80d397953abf6cd73116566f7a56231c
François Liard <lifra05@yahoo.fr>
2024-03-04 23:19:38 +0100

    Add burn function

--- contracts/BearRumble.sol
+++ contracts/BearRumble.sol
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
 
-import "./baseContracts/ERC20.sol";
+import "./baseContracts/ERC20Burnable.sol";
 
-contract BearRumble is ERC20 {
-    constructor() ERC20("BearRumble", "BR") {}
+contract BearRumble is ERC20Burnable {
+    constructor() ERC20("BearRumble", "BR") {
+        _mint(msg.sender, 1_000_000_000 * 10 ** decimals());
+    }
 }
