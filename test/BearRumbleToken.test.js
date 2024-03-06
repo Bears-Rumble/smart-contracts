370d483fc9f4a67de6d30f949fdfc699e7f8ebd6
François Liard <lifra05@yahoo.fr>
2024-03-06 16:16:06 +0100

    Remove .only

--- test/BearRumbleToken.test.js
+++ test/BearRumbleToken.test.js
@@ -1,7 +1,7 @@
 const { ethers } = require("hardhat");
 const { expect } = require("chai");
 
-describe.only("BearRumbleToken", function() {
+describe("BearRumbleToken", function() {
 
     let BearRumbleToken, bearRumbleToken, owner, addr1, addr2;
 
