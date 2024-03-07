dfb2263dd20b702ed4bd5a75212feb11b0fa80d3
François Liard <lifra05@yahoo.fr>
2024-03-07 13:19:06 +0100

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,24 @@
+require("dotenv").config();
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.20",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+    },
+    ethereum: {
+      url: process.env.ETHEREUM_RPC_URL,
+      accounts: [process.env.ETHEREUM_PRIVATE_KEY]
+    }
+  },
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
 };
