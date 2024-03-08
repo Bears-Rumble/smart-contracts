6a36f9f23d20b8f71d3462faa408a6c4af0c082b
François Liard <lifra05@yahoo.fr>
2024-03-08 10:34:35 +0100

    Change data struct, add features

--- contracts/ICO.sol
+++ contracts/ICO.sol
@@ -6,48 +6,70 @@ import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
 import {BearRumble} from "./BearRumble.sol";
 
+/**
+ * @title  ICO Contract
+ * @author IARD Solutions: https://iard.solutions - Web3 Experts suited to your needs
+ * @notice This contract is used to manage the ICO of the BearRumble token
+ *         The ICO has two sales, a cliff period and a vesting period
+ *         The owner can manage the whitelist, end the sale and burn the unsold tokens
+ *         The buyers can buy tokens and claim them after the vesting period
+ */
+
 contract ICO is Ownable, ReentrancyGuard {
     BearRumble token;
 
-    // Sale stages
-    enum SaleStage {
+    /**
+     * @notice Sale details
+     * @param price Price of the token in tokens per Ether
+     * @param tokenSupply Total supply of tokens for the sale in the smallest unit of the token
+     * @param soldTokens Amount of tokens sold in the smallest unit of the token
+     * @param minPurchase Minimum purchase in the smallest unit of the token
+     * @param startTime Timestamp of the start of the sale in seconds
+     * @param endTime Timestamp of the end of the sale in seconds
+     */
+    struct Sale {
+        uint256 price;
+        uint256 tokenSupply;
+        uint256 soldTokens;
+        uint256 minPurchase;
+        uint256 startTime;
+        uint256 endTime;
+    }
+
+    /**
+     * @notice Enum for the sale stages
+     * @param BeforeStart ICO not started yet
+     * @param SaleOne First sale
+     * @param BetweenSaleOneAndTwo Between sale one and two
+     * @param SaleTwo Second sale
+     * @param CliffPeriod Cliff period
+     * @param VestingPeriod Vesting period
+     * @param Ended ICO ended
+     */
+    enum SaleStages {
         BeforeStart,
         SaleOne,
+        BetweenSaleOneAndTwo,
         SaleTwo,
         CliffPeriod,
         VestingPeriod,
         Ended
     }
-    SaleStage public saleStage;
+    SaleStages public saleStage;
 
     // Sale details
+    Sale public saleOne;
+    Sale public saleTwo;
 
-    // Price of the token in tokens per Ether
-    // Example: 1 ETH = 1000 tokens, then the price is 1000
-    uint256 public saleOnePrice;
-    uint256 public saleTwoPrice;
-
-    // Total token supply for each sale
-    uint256 public saleOneTokenSupply;
-    uint256 public saleTwoTokenSupply;
-
-    uint256 public saleOneSoldTokens;
-    uint256 public saleTwoSoldTokens;
-
-    // Timestamps for each sale stage
-    uint256 public saleOneStartTime;
-    uint256 public saleOneEndTime;
-    uint256 public saleTwoStartTime;
-    uint256 public saleTwoEndTime;
-
-    // Whitelist, true if the address is whitelisted
+    // Whitelist, true if the address is whitelisted. Only whitelisted addresses can buy tokens
     mapping(address => bool) public whitelist;
 
     // Vesting
     // Cliff period in seconds
     uint256 public cliffPeriod;
+
     // Vesting period in seconds
-    // 0% of the tokens will be claimable at the start of the vesting period and 100% at the end
+    // 0% of the tokens will be claimable at the start of the vesting period and 100% at the end, linearly unlocking over the period
     uint256 public vestingPeriod;
 
     // Token tracking
@@ -57,36 +79,35 @@ contract ICO is Ownable, ReentrancyGuard {
     // Events
     event TokenPurchased(address buyer, uint256 amount, uint256 price);
     event TokenClaimed(address claimer, uint256 amount);
-    event SaleStageChanged(SaleStage newStage);
+    event SaleStageChanged(SaleStages newStage);
     event TokenBurned(uint256 amount);
 
     constructor(
         address _token,
-        uint256 _saleOnePrice,
-        uint256 _saleTwoPrice,
-        uint256 _saleOneTokenSupply,
-        uint256 _saleTwoTokenSupply,
-        uint256 _saleOneStartTime,
-        uint256 _saleOneEndTime,
-        uint256 _saleTwoStartTime,
-        uint256 _saleTwoEndTime,
+        Sale memory _saleOne,
+        Sale memory _saleTwo,
         uint256 _cliffPeriod,
         uint256 _vestingPeriod
     ) Ownable(msg.sender) {
         token = BearRumble(_token);
-        saleOnePrice = _saleOnePrice;
-        saleTwoPrice = _saleTwoPrice;
-        saleOneTokenSupply = _saleOneTokenSupply;
-        saleTwoTokenSupply = _saleTwoTokenSupply;
-        saleOneStartTime = _saleOneStartTime;
-        saleOneEndTime = _saleOneEndTime;
-        saleTwoStartTime = _saleTwoStartTime;
-        saleTwoEndTime = _saleTwoEndTime;
+
+        saleOne.price = _saleOne.price;
+        saleOne.tokenSupply = _saleOne.tokenSupply;
+        saleOne.minPurchase = _saleOne.minPurchase;
+        saleOne.startTime = _saleOne.startTime;
+        saleOne.endTime = _saleOne.endTime;
+
+        saleTwo.price = _saleTwo.price;
+        saleTwo.tokenSupply = _saleTwo.tokenSupply;
+        saleTwo.minPurchase = _saleTwo.minPurchase;
+        saleTwo.startTime = _saleTwo.startTime;
+        saleTwo.endTime = _saleTwo.endTime;
+
         cliffPeriod = _cliffPeriod;
         vestingPeriod = _vestingPeriod;
 
-        saleOneSoldTokens = 0;
-        saleTwoSoldTokens = 0;
+        saleOne.soldTokens = 0;
+        saleTwo.soldTokens = 0;
 
         whitelist[msg.sender] = true;
     }
@@ -104,10 +125,10 @@ contract ICO is Ownable, ReentrancyGuard {
 
         // Calculate the token price based on the current sale stage
         uint256 price;
-        if (saleStage == SaleStage.SaleOne) {
-            price = saleOnePrice;
-        } else if (saleStage == SaleStage.SaleTwo) {
-            price = saleTwoPrice;
+        if (saleStage == SaleStages.SaleOne) {
+            price = saleOne.price;
+        } else if (saleStage == SaleStages.SaleTwo) {
+            price = saleTwo.price;
         } else {
             revert("Sale not active");
         }
@@ -115,22 +136,32 @@ contract ICO is Ownable, ReentrancyGuard {
         // Check if the buyer has sent enough Ether to purchase the tokens
         require(msg.value == _amount / price, "Wrong amount of Ether sent");
 
-        //Send ETH to the owner
-        payable(owner()).transfer(msg.value);
+        // Check if the amount is greater than the minimum purchase
+        if (saleStage == SaleStages.SaleOne) {
+            require(
+                _amount >= saleOne.minPurchase,
+                "Amount less than minimum purchase"
+            );
+        } else if (saleStage == SaleStages.SaleTwo) {
+            require(
+                _amount >= saleTwo.minPurchase,
+                "Amount less than minimum purchase"
+            );
+        }
 
         //Check if enough tokens are available for the sale and update the sold tokens
-        if (saleStage == SaleStage.SaleOne) {
+        if (saleStage == SaleStages.SaleOne) {
             require(
-                saleOneSoldTokens + _amount <= saleOneTokenSupply,
+                saleOne.soldTokens + _amount <= saleOne.tokenSupply,
                 "Insufficient remaining tokens supply"
             );
-            saleOneSoldTokens += _amount;
-        } else if (saleStage == SaleStage.SaleTwo) {
+            saleOne.soldTokens += _amount;
+        } else if (saleStage == SaleStages.SaleTwo) {
             require(
-                saleTwoSoldTokens + _amount <= saleTwoTokenSupply,
+                saleTwo.soldTokens + _amount <= saleTwo.tokenSupply,
                 "Insufficient remaining tokens supply"
             );
-            saleTwoSoldTokens += _amount;
+            saleTwo.soldTokens += _amount;
         }
 
         // Register the bought tokens
@@ -143,8 +174,8 @@ contract ICO is Ownable, ReentrancyGuard {
     function claimTokens() external onlyWhiteListed nonReentrant {
         setSaleStage();
         require(
-            saleStage == SaleStage.VestingPeriod ||
-                saleStage == SaleStage.Ended,
+            saleStage == SaleStages.VestingPeriod ||
+                saleStage == SaleStages.Ended,
             "Claim period not started"
         );
 
@@ -153,12 +184,12 @@ contract ICO is Ownable, ReentrancyGuard {
         uint256 claimableTokens = 0;
         uint256 unlockedTokens = 0;
 
-        if (saleStage == SaleStage.VestingPeriod) {
+        if (saleStage == SaleStages.VestingPeriod) {
             unlockedTokens =
                 (boughtTokens[msg.sender] *
-                    (currentTime - saleTwoEndTime - cliffPeriod)) /
+                    (currentTime - saleTwo.endTime - cliffPeriod)) /
                 vestingPeriod;
-        } else if (saleStage == SaleStage.Ended) {
+        } else if (saleStage == SaleStages.Ended) {
             unlockedTokens = boughtTokens[msg.sender];
         }
 
@@ -177,23 +208,24 @@ contract ICO is Ownable, ReentrancyGuard {
 
     /************************************ Owner functions ************************************/
 
-    function burnUnsoldTokens() external onlyOwner {
+    function burnUnsoldTokens() public onlyOwner {
         setSaleStage();
 
         require(
-            saleStage == SaleStage.CliffPeriod ||
-                saleStage == SaleStage.VestingPeriod ||
-                saleStage == SaleStage.Ended,
+            saleStage == SaleStages.CliffPeriod ||
+                saleStage == SaleStages.VestingPeriod ||
+                saleStage == SaleStages.Ended,
             "Sale not ended"
         );
 
-        uint256 unsoldTokensSaleOne = saleOneTokenSupply - saleOneSoldTokens;
-        uint256 unsoldTokensSaleTwo = saleTwoTokenSupply - saleTwoSoldTokens;
+        uint256 unsoldTokensSaleOne = saleOne.tokenSupply - saleOne.soldTokens;
+        uint256 unsoldTokensSaleTwo = saleTwo.tokenSupply - saleTwo.soldTokens;
         uint256 unsoldTokens = unsoldTokensSaleOne + unsoldTokensSaleTwo;
-        
-        token.burn(unsoldTokens);
 
-        emit TokenBurned(unsoldTokens);
+        if (unsoldTokens != 0) {
+            token.burn(unsoldTokens);
+            emit TokenBurned(unsoldTokens);
+        }
     }
 
     function manageWhitelist(
@@ -209,36 +241,63 @@ contract ICO is Ownable, ReentrancyGuard {
         }
     }
 
+    function endSale() external onlyOwner {
+        setSaleStage();
+        require(
+            saleStage == SaleStages.BetweenSaleOneAndTwo ||
+                saleStage > SaleStages.SaleTwo,
+            "Cannot end the sale yet"
+        );
+
+        if (saleStage == SaleStages.BetweenSaleOneAndTwo) {
+            // After sale one, move the unsold tokens from sale one to sale two
+            uint256 unsoldTokensSaleOne = saleOne.tokenSupply -
+                saleOne.soldTokens;
+            saleTwo.tokenSupply += unsoldTokensSaleOne;
+            saleOne.tokenSupply = saleOne.soldTokens;
+
+            // Send the sale benefits to the owner
+            payable(owner()).transfer(address(this).balance);
+        } else {
+            // After sale two, burn the unsold tokens
+            burnUnsoldTokens();
+
+            // Send the sale benefits to the owner
+            payable(owner()).transfer(address(this).balance);
+        }
+    }
+
     /************************************ Internal functions ************************************/
 
     function setSaleStage() internal {
         uint256 currentTime = block.timestamp;
 
-        if (
-            currentTime < saleOneStartTime ||
-            (currentTime > saleOneEndTime && currentTime < saleTwoStartTime)
-        ) {
-            saleStage = SaleStage.BeforeStart;
+        if (currentTime < saleOne.startTime) {
+            saleStage = SaleStages.BeforeStart;
         } else if (
-            currentTime >= saleOneStartTime && currentTime <= saleOneEndTime
+            currentTime >= saleOne.startTime && currentTime <= saleOne.endTime
         ) {
-            saleStage = SaleStage.SaleOne;
+            saleStage = SaleStages.SaleOne;
         } else if (
-            currentTime >= saleTwoStartTime && currentTime < saleTwoEndTime
+            currentTime > saleOne.endTime && currentTime < saleTwo.startTime
         ) {
-            saleStage = SaleStage.SaleTwo;
+            saleStage = SaleStages.BetweenSaleOneAndTwo;
         } else if (
-            currentTime >= saleTwoEndTime &&
-            currentTime < saleTwoEndTime + cliffPeriod
+            currentTime >= saleTwo.startTime && currentTime < saleTwo.endTime
         ) {
-            saleStage = SaleStage.CliffPeriod;
+            saleStage = SaleStages.SaleTwo;
         } else if (
-            currentTime >= saleTwoEndTime + cliffPeriod &&
-            currentTime < saleTwoEndTime + cliffPeriod + vestingPeriod
+            currentTime >= saleTwo.endTime &&
+            currentTime < saleTwo.endTime + cliffPeriod
         ) {
-            saleStage = SaleStage.VestingPeriod;
+            saleStage = SaleStages.CliffPeriod;
+        } else if (
+            currentTime >= saleTwo.endTime + cliffPeriod &&
+            currentTime < saleTwo.endTime + cliffPeriod + vestingPeriod
+        ) {
+            saleStage = SaleStages.VestingPeriod;
         } else {
-            saleStage = SaleStage.Ended;
+            saleStage = SaleStages.Ended;
         }
     }
 
