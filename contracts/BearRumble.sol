c9903577d13c1c13a8f4cf6279d75576450eceac
François Liard <lifra05@yahoo.fr>
2024-06-20 16:53:00 +0200

    Update name and comments

--- contracts/BearRumble.sol
+++ contracts/BearRumble.sol
@@ -1,25 +1,25 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
-
+ 
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-
+ 
 /**
- * @title  Bear Rumble ERC20 Contract
+ * @title  Bears Rumble ERC20 Contract
  * @author IARD Solutions: https://iard.solutions - Web3 Experts suited to your needs. Web3 | Consulting | Innovations
- * @author Bear Rumble: 
- * @notice This contract is the ERC20 token contract for Bear Rumble. 
+ * @author Bears Rumble:
+ * @notice This contract is the ERC20 token contract for Bears Rumble.
  *          It is based on OpenZeppelin's ERC20Burnable contract.
  *
- *  BearRumble is a Play2Earn & Free2Play multiplayer Web3 Game
+ *  BearsRumble is a Play2Earn & Free2Play multiplayer Web3 Game
  *  Socials:
- *  - Website:
- *  - Twitter:
- *  - Telegram:
- *  - Discord:
+ *  - Website:https://bearsrumble.com/
+ *  - Twitter:https://x.com/BearsRumble
+ *  - Telegram:https://t.me/BearsRumble
+ *  - Discord:https://discord.com/invite/bearsrumble
  */
-
-contract BearRumble is ERC20Burnable {
-    constructor() ERC20("BearRumble", "BR") {
+ 
+contract BearsRumble is ERC20Burnable {
+    constructor() ERC20("BearsRumble", "BR") {
         _mint(msg.sender, 1_000_000_000 * 10 ** decimals());
     }
 }
