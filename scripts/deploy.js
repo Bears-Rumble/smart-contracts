47b4af0a64c1f7ded00bf63a428cc0400542581d
François Liard <56561415+Pacdac@users.noreply.github.com>
2024-09-06 11:00:33 +0200

    Update deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,30 +6,32 @@ async function main() {
   const saleOnePrice = 130_000; // Price in Tokens per Ether
   const saleOneSupply = 40_000_000n * 10n ** 18n;
   const saleOneMinPurchase = 20_000n * 10n ** 18n; // About 500 USD  
-  const saleOneStart = Math.round(Date.now() / 1000) + 60 * 60 * 24 * 7; // 7 days from now
-  const saleOneEnd = saleOneStart + 60 * 60 * 24 * 30; // 30 days
+  const saleOneStart = Math.round(Date.now() / 1000); // Now
+  const saleOneEnd = saleOneStart + 60 * 60 * 24 * 30; // 4 days
   const saleOneMinTokensSold = 140_000n * 10n ** 18n;
 
   const saleTwoPrice = 60_000; // Price in Tokens per Ether
   const saleTwoSupply = 50_000_000n * 10n ** 18n;
   const saleTwoMinPurchase = 15_000n * 10n ** 18n; // About 200 USD
-  const saleTwoStart = saleOneEnd + 60 * 60 * 24 * 60; // 60 days after SaleOne ends
-  const saleTwoEnd = saleTwoStart + 60 * 60 * 24 * 30; // 30 days
+  const saleTwoStart = saleOneEnd + 60 * 60 * 24 ; // 1 days after SaleOne ends
+  const saleTwoEnd = saleTwoStart + 60 * 60 * 24 * 3; // 3 days
   const saleTwoMinTokensSold = 70_000n * 10n ** 18n;
 
   const saleThreePrice = 30_000; // Price in Tokens per Ether
   const saleThreeSupply = 60_000_000n * 10n ** 18n;
   const saleThreeMinPurchase = 10_000n * 10n ** 18n;
-  const saleThreeStart = saleTwoEnd + 60 * 60 * 24 * 60; // 30 days after SaleTwo ends
-  const saleThreeEnd = saleThreeStart + 60 * 60 * 24 * 30; // 30 days
+  const saleThreeStart = saleTwoEnd + 60 * 60 * 6; // 6 hours after SaleTwo ends
+  const saleThreeEnd = saleThreeStart + 60 * 60 * 24 * 1; // 1 day
   const saleThreeMinTokensSold = 10_000_000n * 10n ** 18n;
 
-  const cliffPeriod = 60 * 60 * 24 * 30; // 30 days
-  const vestingPeriod = 60 * 60 * 24 * 300; // 300 days
+  const cliffPeriod = 60 * 60 * 24 * 1; // 1 days
+  const vestingPeriod = 60 * 60 * 24 * 10; // 10 days
 
   const saleTwo = [saleTwoPrice, saleTwoSupply, 0, saleTwoMinPurchase, saleTwoStart, saleTwoEnd, saleTwoMinTokensSold, false, false];
   const saleOne = [saleOnePrice, saleOneSupply, 0, saleOneMinPurchase, saleOneStart, saleOneEnd, saleOneMinTokensSold, false, false];
   const saleThree = [saleThreePrice, saleThreeSupply, 0, saleThreeMinPurchase, saleThreeStart, saleThreeEnd, saleThreeMinTokensSold, false, false];
+
+  const referralRate = 40; // 1/40 = 2.5%
 
   // Deploy contracts
   let deployer;
@@ -39,8 +41,6 @@ async function main() {
   }
   [deployer] = await hre.ethers.getSigners();
   console.log("Deploying contracts with the account:", deployer.address);
-
-  console.log("Account balance:", (await deployer.getBalance()).toString());
 
   const BearsRumbleToken = await hre.ethers.getContractFactory("BearsRumble");
   const bearsRumbleToken = await BearsRumbleToken.deploy();
@@ -56,7 +56,8 @@ async function main() {
     saleTwo,
     saleThree,
     cliffPeriod,
-    vestingPeriod
+    vestingPeriod,
+    referralRate
   );
 
   await bearsRumbleICO.waitForDeployment();
