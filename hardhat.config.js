dd068faf36ee4a2618996c1aaf76a4f8eb5a696f
François Liard <56561415+Pacdac@users.noreply.github.com>
2024-09-15 10:38:58 +0200

    Add tasks

--- hardhat.config.js
+++ hardhat.config.js
@@ -25,10 +25,33 @@ task("manageWhitelist", "Whitelists an address")
   .addParam("addresses", "The address to be whitelisted")
   .addParam("isWhitelisted", "Whether the address is whitelisted")
   .setAction(async (taskArgs) => {
-    const addresses = taskArgs.addresses;
-    const isWhitelisted = taskArgs.isWhitelisted;
-    console.log("Whitelisting addresses: ", addresses);
-    console.log("Is whitelisted: ", isWhitelisted);
+    let addresses = taskArgs.addresses;
+    let isWhitelisted = taskArgs.isWhitelisted;
+    
+    let contractAddress;
+    if (hre.network.name === "ethereum") {
+      contractAddress = process.env.ETHEREUM_ICO_CONTRACT_ADDRESS;
+    } else if (hre.network.name === "bnbTestnet") {
+      contractAddress = process.env.BNB_TESTNET_ICO_CONTRACT_ADDRESS;
+    }
+
+    // Get the contract with the factory
+    const ICOContract = await hre.ethers.getContractFactory("ICO");
+    const icoContract = ICOContract.attach(contractAddress);
+    console.log(`Managing whitelist for contract at address ${contractAddress}`);
+    // Whitelist addresses
+    const trx = await icoContract.manageWhitelist(["0x21C2b58eB7Fe8497fdb2864c50eA095621295738"], [true]);
+    
+    // Log the result
+    console.log(trx);
+
+    console.log(`Done!`);
+  });
+
+task("setWhitelistManager", "Sets a whitelist manager")
+  .addParam("address", "The address to be added as a whitelist manager")
+  .setAction(async (taskArgs) => {
+    const address = taskArgs.address;
     
     let contractAddress;
     if (hre.network.name === "ethereum") {
@@ -41,10 +64,8 @@ task("manageWhitelist", "Whitelists an address")
     const ICOContract = await hre.ethers.getContractFactory("ICO");
     const icoContract = ICOContract.attach(contractAddress);
 
-    // Whitelist addresses
-    await icoContract.manageWhitelist(addresses, isWhitelisted);
-
-    console.log(`Done!`);
+    const trx = await icoContract.setWhitelistManager(address);
+    console.log(trx);
   });
 
 /** @type import('hardhat/config').HardhatUserConfig */
